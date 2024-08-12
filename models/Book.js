036fed6686f22d9036de5940622ba35e7e98dac2
RizkySyahrani <syahranirizky99@gmail.com>
2024-08-12 09:28:47 +0700

    Upload Project

---
+++ models/Book.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  publishedDate: {
+    type: Date,
+  },
+  pages: {
+    type: Number,
+  },
+  genre: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("Book", bookSchema);
